f4fb65df6ed54da1c84050b42dce64abb55a0597
Milen Kirilov <savemetenminutes@gmail.com>
2018-05-27 20:58:46 +0300

    Updates

--- src/AliasesToClassMethodsHydrator.php
+++ src/AliasesToClassMethodsHydrator.php
@@ -6,25 +6,44 @@ use Zend\Hydrator\ClassMethods;
 
 class AliasesToClassMethodsHydrator extends ClassMethods
 {
-    protected $aliases;
+    protected $mapHydration;
+    protected $mapExtraction;
 
-    public function __construct($underscoreSeparatedKeys = true, array $aliases = [])
+    public function __construct($underscoreSeparatedKeys = true, array $mapExtraction = [], ?array $mapHydration = [])
     {
-        $this->aliases = $aliases;
         parent::__construct($underscoreSeparatedKeys);
+
+        $this->mapExtraction = ($mapExtraction === null) ? array_flip($mapHydration) : $mapExtraction;
+        $this->mapHydration = ($mapHydration === null) ? array_flip($mapExtraction) : $mapHydration;
     }
 
     public function hydrate(array $data, $object)
     {
         $reindexedData = [];
         foreach ($data as $key => $datum) {
-            if (isset($this->aliases[$key])) {
-                $reindexedData[$this->aliases[$key]] = $datum;
+            if (isset($this->mapHydration[$key])) {
+                $reindexedData[$this->mapHydration[$key]] = $datum;
             } else {
                 $reindexedData[$key] = $datum;
             }
         }
 
         return parent::hydrate($reindexedData, $object);
+    }
+
+    public function extract($object)
+    {
+        $data = parent::extract($object);
+
+        $reindexedData = [];
+        foreach ($data as $key => $datum) {
+            if (isset($this->mapExtraction[$key])) {
+                $reindexedData[$this->mapExtraction[$key]] = $datum;
+            } else {
+                $reindexedData[$key] = $datum;
+            }
+        }
+
+        return $reindexedData;
     }
 }
